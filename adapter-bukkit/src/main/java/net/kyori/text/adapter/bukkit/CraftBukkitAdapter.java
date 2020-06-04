9e30537dc1ad2878760e2a259ad1fe429c40f1ec
Zach Levis <zml@aoeu.xyz>
2020-06-04 12:37:24 -0700

    Prepare for MC 1.16 chat packet changes
    not yet tested on 1.16-pre

--- adapter-bukkit/src/main/java/net/kyori/text/adapter/bukkit/CraftBukkitAdapter.java
+++ adapter-bukkit/src/main/java/net/kyori/text/adapter/bukkit/CraftBukkitAdapter.java
@@ -32,12 +32,14 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 import java.util.function.Function;
 import net.kyori.text.Component;
 import net.kyori.text.serializer.gson.GsonComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 final class CraftBukkitAdapter implements Adapter {
   private static final Binding REFLECTION_BINDINGS = load();
@@ -58,16 +60,30 @@ final class CraftBukkitAdapter implements Adapter {
       final Class<?> packetClass = minecraftClass(serverVersion, "Packet");
       final Method sendPacketMethod = playerConnectionClass.getMethod("sendPacket", packetClass);
       final Class<?> baseComponentClass = minecraftClass(serverVersion, "IChatBaseComponent");
+      final Class<?> chatMessageTypeClass = optionalMinecraftClass(serverVersion, "ChatMessageType");
+      final Object chatMessageTypeSystem = enumValue(chatMessageTypeClass, "SYSTEM", 1);
       final Class<?> chatPacketClass = minecraftClass(serverVersion, "PacketPlayOutChat");
-      final Constructor<?> chatPacketConstructor = chatPacketClass.getConstructor(baseComponentClass);
+
+      boolean longConstructor = false;
+      Constructor<?> chatPacketConstructor;
+      try {
+        chatPacketConstructor = chatPacketClass.getConstructor(baseComponentClass);
+      } catch(NoSuchMethodException ex) { // 1.16 chat packet change
+        if(chatMessageTypeClass == null) {
+          throw ex;
+        }
+        longConstructor = true;
+        chatPacketConstructor = chatPacketClass.getConstructor(baseComponentClass, chatMessageTypeClass, UUID.class);
+      }
       final Class<?> titlePacketClass = optionalMinecraftClass(serverVersion, "PacketPlayOutTitle");
-      final Class<? extends Enum> titlePacketClassAction;
+      final Object titleActionActionBar;
       final Constructor<?> titlePacketConstructor;
       if(titlePacketClass != null) {
-        titlePacketClassAction = (Class<? extends Enum>) minecraftClass(serverVersion, "PacketPlayOutTitle$EnumTitleAction");
+        Class<?> titlePacketClassAction = minecraftClass(serverVersion, "PacketPlayOutTitle$EnumTitleAction");
+        titleActionActionBar = enumValue(titlePacketClassAction, "ACTIONBAR", 2);
         titlePacketConstructor = titlePacketClass.getConstructor(titlePacketClassAction, baseComponentClass);
       } else {
-        titlePacketClassAction = null;
+        titleActionActionBar = null;
         titlePacketConstructor = null;
       }
       final Class<?> chatSerializerClass = Arrays.stream(baseComponentClass.getClasses())
@@ -87,7 +103,7 @@ final class CraftBukkitAdapter implements Adapter {
         .filter(m -> m.getParameterCount() == 1 && m.getParameterTypes()[0].equals(String.class))
         .min(Comparator.comparing(Method::getName)) // prefer the #a method
         .orElseThrow(() -> new RuntimeException("Unable to find serialize method"));
-      return new AliveBinding(getHandleMethod, playerConnectionField, sendPacketMethod, chatPacketConstructor, titlePacketClassAction, titlePacketConstructor, serializeMethod);
+      return new AliveBinding(getHandleMethod, playerConnectionField, sendPacketMethod, chatMessageTypeSystem, chatPacketConstructor, titleActionActionBar, titlePacketConstructor, serializeMethod, longConstructor);
     } catch(final Throwable e) {
       return new DeadBinding();
     }
@@ -191,24 +207,30 @@ final class CraftBukkitAdapter implements Adapter {
   }
 
   private static final class AliveBinding extends Binding {
+    private static final UUID NIL_UUID = new UUID(0, 0);
+
     private final Method getHandleMethod;
     private final Field playerConnectionField;
     private final Method sendPacketMethod;
+    private final Object chatMessageTypeSystem;
     private final Constructor<?> chatPacketConstructor;
-    private final Class<? extends Enum> titlePacketClassAction;
+    private final Object titlePacketActionActionBar;
     private final Constructor<?> titlePacketConstructor;
     private final boolean canMakeTitle;
     private final Method serializeMethod;
+    private final boolean longChatPacketConstructor;
 
-    AliveBinding(final Method getHandleMethod, final Field playerConnectionField, final Method sendPacketMethod, final Constructor<?> chatPacketConstructor, final Class<? extends Enum> titlePacketClassAction, final Constructor<?> titlePacketConstructor, final Method serializeMethod) {
+    AliveBinding(final Method getHandleMethod, final Field playerConnectionField, final Method sendPacketMethod, final Object chatMessageTypeSystem, final Constructor<?> chatPacketConstructor, final Object titlePacketActionActionBar, final Constructor<?> titlePacketConstructor, final Method serializeMethod, final boolean longChatPacketConstructor) {
       this.getHandleMethod = getHandleMethod;
       this.playerConnectionField = playerConnectionField;
       this.sendPacketMethod = sendPacketMethod;
+      this.chatMessageTypeSystem = chatMessageTypeSystem;
       this.chatPacketConstructor = chatPacketConstructor;
-      this.titlePacketClassAction = titlePacketClassAction;
       this.titlePacketConstructor = titlePacketConstructor;
-      this.canMakeTitle = this.titlePacketClassAction != null && this.titlePacketConstructor != null;
+      this.titlePacketActionActionBar = titlePacketActionActionBar;
+      this.canMakeTitle = this.titlePacketConstructor != null && this.titlePacketActionActionBar != null;
       this.serializeMethod = serializeMethod;
+      this.longChatPacketConstructor = longChatPacketConstructor;
     }
 
     @Override
@@ -220,7 +242,12 @@ final class CraftBukkitAdapter implements Adapter {
     Object createMessagePacket(final Component component) {
       final String json = GsonComponentSerializer.INSTANCE.serialize(component);
       try {
-        return this.chatPacketConstructor.newInstance(this.serializeMethod.invoke(null, json));
+        final Object mc = this.serializeMethod.invoke(null, json);
+        if(longChatPacketConstructor) {
+          return this.chatPacketConstructor.newInstance(mc, this.chatMessageTypeSystem, NIL_UUID);
+        } else {
+          return this.chatPacketConstructor.newInstance(mc);
+        }
       } catch(final Exception e) {
         throw new UnsupportedOperationException("An exception was encountered while creating a packet for a component", e);
       }
@@ -230,14 +257,8 @@ final class CraftBukkitAdapter implements Adapter {
     Object createActionBarPacket(final Component component) {
       if(this.canMakeTitle) {
         try {
-          Enum constant;
-          try {
-            constant = Enum.valueOf(this.titlePacketClassAction, "ACTIONBAR");
-          } catch(final IllegalArgumentException e) {
-            constant = this.titlePacketClassAction.getEnumConstants()[2];
-          }
           final String json = GsonComponentSerializer.INSTANCE.serialize(component);
-          return this.titlePacketConstructor.newInstance(constant, this.serializeMethod.invoke(null, json));
+          return this.titlePacketConstructor.newInstance(this.titlePacketActionActionBar, this.serializeMethod.invoke(null, json));
         } catch(final Exception e) {
           throw new UnsupportedOperationException("An exception was encountered while creating a packet for a component", e);
         }
@@ -255,5 +276,25 @@ final class CraftBukkitAdapter implements Adapter {
         throw new UnsupportedOperationException("An exception was encountered while sending a packet for a component", e);
       }
     }
+  }
+
+  @SuppressWarnings("unchecked") // intellij lies, it's needed
+  private static @Nullable Object enumValue(final @Nullable Class<?> klass, String name, int ordinal) {
+    if(klass == null) {
+      return null;
+    }
+    if(!Enum.class.isAssignableFrom(klass)) {
+      return null;
+    }
+
+    try {
+      return Enum.valueOf(klass.asSubclass(Enum.class), name);
+    } catch(IllegalArgumentException ex) {
+      final Object[] constants = klass.getEnumConstants();
+      if(constants.length > ordinal) {
+        return ordinal;
+      }
+    }
+    return null;
   }
 }
